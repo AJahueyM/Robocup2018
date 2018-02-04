8ee7b4cb26e77ed2fbeb1dcd7c32d000ace25201
YulisaM2 <a01570056@itesm.mx>
2018-02-04 16:08:17 -0600

    Pruebas bno 055
    Pruebas en main con bno
    Falta
    -Reestablecer valor a 0 (corregir que empiece en valores randoms)
    -Crear Clase

--- src/Main.ino
+++ src/Main.ino
@@ -3,15 +3,40 @@
 
 using namespace std;
 
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
 void setup(){
 
-Serial.begin(9600);
-Serial.println("INICIAR PROG");
+	Serial.begin(9600);
+	Serial.println("INICIAR PROG");
+
+	if(!bno.begin()){
+		Serial.print("NO BNO 055 DETECTED");
+		while(1);
+		}
+
+	delay(5000);
+
+	for(int i = 0; i < 6; )
+
+	bno.setExtCrystalUse(true);
+}
+
+
+void loop(void){
+
+	sensors_event_t event;
+	bno.getEvent(&event);
+	
+	Serial.print("X ");
+	Serial.print(event.orientation.x, 4);
+	Serial.print("\tY ");
+	Serial.print(event.orientation.y, 4);
+	Serial.print("\tZ ");
+	Serial.print(event.orientation.z, 4);
+	Serial.println("");
+
+	delay(100);
 
 }
 
-void loop(){
-
-
-
-}
