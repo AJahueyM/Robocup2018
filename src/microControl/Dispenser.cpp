90f249c85758c53594e8a5f0a9abe8b2b73b2f1b
YulisaM2 <a01570056@itesm.mx>
2018-02-21 17:34:01 -0600

    Class Dispenser Update
    Addition of .cpp

---
+++ src/microControl/Dispenser.cpp
@@ -0,0 +1,47 @@
+#include "Dispenser.h"
+
+Dispenser::Dispenser(int pin){
+	servoDispenser.attach(pin);
+}
+
+Dispenser::Dispenser(int startAmount){
+	amountAvailable = startAmount;
+}
+void Dispenser::setAmountAvailable(int iAmount){
+	amountAvailable = iAmount;
+}
+int Dispenser::getAmountAvailable(){
+	return amountAvailable;
+}
+int Dispenser::updateAmountAvailable(){
+	amountAvailable--;
+	return amountAvailable;
+}
+
+void Dispenser::dispenseDirection(DispenserDirection direction, int amount){
+	if(kitsAvailable()){
+		if(direction == left){
+			for(int i =0; i< amount; i++){
+				servoDispenser.write(45);
+				delay(1000);
+				servoDispenser.write(90);
+				servoDispenser.updateAmountAvailable(1);
+			}
+		}else if(direction == right){
+			for(int i =0; i<amount; i++){
+				servoDispenser.write(135);
+				delay(1000);
+				servoDispenser.write(90);
+				servoDispenser.updateAmountAvailable(1)
+			}
+		}else if(direction == center)
+			servoDispenser.write(90);
+	}
+}
+
+bool Dispenser::kitsAvailable(){
+	if(getAmountAvailable() > 0)
+		return true;
+	else 
+		return false;
+}
--- src/microControl/subsystems/Dispenser.h
+++ src/microControl/subsystems/Dispenser.h
@@ -7,6 +7,7 @@ right, left, center;
 class Dispenser {
 private:
 	int amountAvailable, startAmount = 12;
+	Servo servoDispenser;
 public:
 	Dispenser(int pin);
 	Dispenser(int startAmount);
@@ -16,47 +17,3 @@ public:
 	void dispenseDirection(DispenserDirection direction, int amount);
 	bool kitsAvailable(int startAmount, int amountAvailable);
 }; 
-
-Dispenser::Dispenser(int pin){
-}
-Dispenser::Dispenser(int startAmount){
-	amountAvailable = startAmount;
-}
-void Dispenser::setAmountAvailable(int iAmount){
-	amountAvailable = iAmount;
-}
-int Dispenser::getAmountAvailable(){
-	return amountAvailable;
-}
-int Dispenser::updateAmountAvailable(){
-	amountAvailable--;
-	return amountAvailable;
-}
-
-void Dispenser::dispenseDirection(DispenserDirection direction, int amount){
-	if(kitsAvailable()){
-		if(direction == left){
-			for(int i =0; i< amount; i++){
-				servoDispenserwrite(45);
-				delay(1000);
-				servoDispenser.write(90);
-				servoDispenser.updateAmountAvailable(1);
-			}
-		}else if(direction == right){
-			for(int i =0; i<amount; i++){
-				servoDispenser.write(135);
-				delay(1000);
-				servoDispenser.write(90);
-				servoDispenser.updateAmountAvailable(1)
-			}
-		}else if(direction == center)
-			servoDispenser.write(90);
-	}
-}
-
-bool Dispenser::kitsAvailable(){
-	if(getAmountAvailable() > 0)
-		return true;
-	else 
-		return false;
-}
