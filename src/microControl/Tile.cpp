2cc88bdfba3b7ab3a3137589a9e8064839bbf686
Alberto <albertojahueymoncada@gmail.com>
2018-03-01 23:13:50 -0600

    Changed Tile to only use one byte

--- src/microControl/Tile.cpp
+++ src/microControl/Tile.cpp
@@ -5,11 +5,28 @@ Tile::Tile(TileConfig tile){
 }
 
 bool Tile::isRamp(){
-
+    byte result = identity & isRampMask;
+    return result != 0;
 }
 
 bool Tile::wallExists(Direction side){
+    byte result;
+    switch(side){
+       	case Up:
+           result = identity & wallUpMask;
+           break;
+        case Down:
+            result = identity & wallDownMask;
+            break;
+        case Left:
+            result = identity & wallLeftMask;
+            break;
 
+        case Right:
+            result = identity & wallRightMask;
+        break;
+    }
+    return result != 0;
 }
 
 bool Tile::heatVictimExists(Direction side){
@@ -21,7 +38,14 @@ bool Tile::visionVictimExists(Direction side){
 }
 
 TileColor Tile::getColor(){
-
+    if(identity & isBlackMask != 0){
+        return Black;
+    }
+    if(identity & isCheckpointMask != 0){
+        return Silver;
+    }
+    else
+    return White;
 }
 
 double Tile::getBumpWeight(){
--- src/microControl/Tile.h
+++ src/microControl/Tile.h
@@ -25,11 +25,19 @@ struct TileConfig {
 
 class Tile {
 public:
+	static const byte wallUpMask = B10000000;
+	static const byte wallRightMask = B01000000;
+	static const byte wallDownMask = B00100000;
+	static const byte wallLeftMask = B00010000;
+	static const byte isRampMask = B00001000;
+	static const byte hasVictimMask = B00000100;
+	static const byte isBlackMask = B00000010;
+	static const byte isCheckpointMask = B0000001;
+
 	Tile(TileConfig config);
 	bool isRamp();
 	bool wallExists(Direction side);
-	bool heatVictimExists(Direction side);
-	bool visionVictimExists(Direction side);
+	bool hasVictim();
 	TileColor getColor();
 	double getBumpWeight();
 private:
@@ -37,6 +45,21 @@ private:
 	TileBump bump;
 	TileColor color;
 	bool isTileRamp;
+	byte identity = B00000000;
 };
 
 #endif
+
+
+
+/*
+	00000000
+	1: wall up
+	2: wall right
+	3: wall down
+	4: wall left
+	5: ramp
+	6: has victim 
+	7: isBlack
+	8: isCheckpoint
+*/
